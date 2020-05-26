c81dbd02760299ed3fe2b2433756d46b39adb4da
ismailkasan <ismailkasan@piton.com.tr>
2020-05-26 23:22:11 +0300

    first commit

---
+++ LocalizationExample/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LocalizationExample.Resources;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace LocalizationExample.Controllers
+{
+    [ApiController]
+    [Route("{culture:culture}/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly IStringLocalizer<Resource> Localizer;
+        public HomeController(IStringLocalizer<Resource> stringLocalizer)
+        {
+            this.Localizer = stringLocalizer;
+        }
+       
+        public string Get()
+        {
+            return Localizer["Home"];
+        }
+    }
+}
